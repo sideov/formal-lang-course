1b027d179d2a665b2d9e9ffc7380fc7a5b607518
MAC <90581598+sideov@users.noreply.github.com>
2024-05-19 17:42:36 +0300

    done

--- project/task9.py
+++ project/task9.py
@@ -1,10 +1,17 @@
+from networkx import DiGraph
+
 from project.task8 import cfg_to_rsm
 
-from pyformlang.cfg import CFG
-import networkx as nx
-from pyformlang.finite_automaton import Symbol, State
-from copy import deepcopy
 from pyformlang.rsa import RecursiveAutomaton
+from pyformlang.cfg import CFG
+from pyformlang.finite_automaton import State, Symbol
+
+import networkx as nx
+
+from typing import *
+from copy import deepcopy
+from itertools import product
+import pyformlang
 
 
 def cfpq_with_gll(
@@ -13,7 +20,6 @@ def cfpq_with_gll(
     start_nodes: set[int] = None,
     final_nodes: set[int] = None,
 ) -> set[tuple[int, int]]:
-
     if isinstance(rsm, CFG):
         rsm = cfg_to_rsm(rsm)
 
@@ -23,7 +29,13 @@ def cfpq_with_gll(
     if final_nodes is None:
         final_nodes = graph.nodes
 
-    rsm_start_nonterm = rsm.initial_label.value
+    rsm_start_nonterm = "S"
+    if rsm.initial_label.value is not None:
+        rsm_start_nonterm = rsm.initial_label.value
+
+    rsm_start = rsm.boxes[rsm.initial_label].dfa.start_state.value
+
+    res = set()
 
     stack_start_states = set()
     for sn in start_nodes:
@@ -33,76 +45,34 @@ def cfpq_with_gll(
     for state in stack_start_states:
         stack[state] = set()
 
-    rsm_start = rsm.boxes[rsm.initial_label].dfa.start_state.value
-
     visited = set()
-    for state in stack_start_states:
-        visited.add(((rsm_start_nonterm, rsm_start), state[1], state))
+    for v in start_nodes:
+        visited.add((v, (v, rsm_start_nonterm)))
 
-    res = set()
-    removed = {}
+    start_dfa = rsm.boxes[rsm_start_nonterm].dfa.to_dict()
+    start_dfa.setdefault(State(rsm_start), dict())
+
     to_visit = deepcopy(visited)
 
-    while len(to_visit) != 0:
-        rsm_state, graph_node, stack_state = to_visit.pop()
+    while len(to_visit) > 0:
 
-        if State(rsm_state[1]) in rsm.boxes[rsm_state[0]].dfa.final_states:
-            if stack_state in stack_start_states:
-                if graph_node in final_nodes:
-                    res.add((stack_state[1], graph_node))
+        v, (node, label) = to_visit.pop()
+        cond = (node, label)
 
-            if stack_state not in removed.keys():
-                removed[stack_state] = set()
-            removed[stack_state].add(graph_node)
-            for to_stack_state, to_rsm_state in stack.setdefault(stack_state, set()):
-                new = (to_rsm_state, graph_node, to_stack_state)
-                if new not in visited:
-                    to_visit.add(new)
-                    visited.add(new)
+        if node in start_nodes and label == rsm_start and v in final_nodes:
+            res.add((node, v))
+        for rsm_state_, stack_state_ in stack.setdefault(cond, set()):
+            s = (v, stack_state_)
+            if s not in visited:
+                visited.add(s)
+                to_visit.add(s)
 
-        to_nodes = {}
-        for q, a, b in graph.edges(graph_node, data="label"):
-            if b not in to_nodes.keys():
-                to_nodes[b] = set()
-            to_nodes[b].add(a)
+        for symbol, _ in start_dfa.items():
+            if symbol in rsm.labels:
+                stack_state_ = (v, symbol.value)
+                s = (v, stack_state_)
+                if s not in visited:
+                    visited.add(s)
+                    to_visit.add(s)
 
-        dfa_dict = rsm.boxes[Symbol(rsm_state[0])].dfa.to_dict()
-        if State(rsm_state[1]) not in dfa_dict:
-            continue
-        for symb, to in dfa_dict[State(rsm_state[1])].items():
-            if symb in rsm.labels:
-                new_state_on_stack = (symb.value, graph_node)
-                if new_state_on_stack in removed:
-                    for to_graph_node in removed[new_state_on_stack]:
-                        new = (
-                            (rsm_state[0], to.value),
-                            to_graph_node,
-                            stack_state,
-                        )
-                        if new not in visited:
-                            to_visit.add(new)
-                            visited.add(new)
-
-                if new_state_on_stack not in stack.keys():
-                    stack[new_state_on_stack] = set()
-                stack[new_state_on_stack].add((stack_state, (rsm_state[0], to.value)))
-
-                start_state = rsm.boxes[symb].dfa.start_state.value
-                new = ((symb.value, start_state), graph_node, new_state_on_stack)
-                if new not in visited:
-                    to_visit.add(new)
-                    visited.add(new)
-            else:
-                if symb.value not in to_nodes:
-                    continue
-                for node in to_nodes[symb.value]:
-                    new = ((rsm_state[0], to.value), node, stack_state)
-                    if new not in visited:
-                        to_visit.add(new)
-                        visited.add(new)
-
-    ans = set()
-    for i in res:
-        if i[0] != i[1]:
-            ans.add(i)
-    return ans
+    return res
